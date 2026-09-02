0771380c523ef1c1181dff0165decf31f07f621f
Mohamad Hafid Risqina <hfdrisqi@gmail.com>
2026-09-02 20:29:30 +0700

    Mengimplementasikan skrip InputManager dan PlayerMovement ke karakter didalam scene

---
+++ Assets/GAME/Resources/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _walkSpeed;
+    [SerializeField]
+    private InputManager _input;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _input.OnMoveInput += Move;
+    }
+
+    private void OnDestroy()
+    {
+        _input.OnMoveInput -= Move;
+    }
+
+    private void Move(Vector2 axisDirection)
+        {
+            Vector3 movementDirection = new Vector3(axisDirection.x, 0, axisDirection.y);
+            _rigidbody.AddForce(movementDirection * _walkSpeed * Time.deltaTime);
+        }
+}
